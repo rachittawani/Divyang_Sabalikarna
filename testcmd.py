d4d880289b3cb2de9b56299fd25f3020d123ffd7
krishna-daga <krishna20299@gmail.com>
2022-06-15 16:34:19 +0530

    done

---
+++ testcmd.py
@@ -0,0 +1,6 @@
+# import os
+# os.system('jupyter notebook')
+# os.system('runipy MyNotebook.ipynb')
+# # os.system('jupyter nbconvert --to notebook --inplace --execute trial.ipynb')
+import os
+os.system('jupyter nbconvert --to html trial.ipynb')
